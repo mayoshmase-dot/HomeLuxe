b1f86f3a9cede5e139eb3d968460b8575e4cbb7e
mayoshmase-dot <mayoshmase@gmail.com>
2026-02-06 08:18:58 +0200

    create asp

---
+++ src/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import Register from "./pages/auth/register/Register";
+import Login from "./pages/auth/login/Login";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: 'cart',
+                element: <Cart />
+            },
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'register',
+                element: <Register />
+            }
+        ]
+    }
+]);
+export default router;
